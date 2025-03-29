f82cf25ef3caea623b67bc4216f0834f43ae7cf3
Raj-Kumar <rajkumarpandit436@gmail.com>
2025-03-29 19:08:08 +0530

    third commit

--- src/main/java/com/jenkins_test/JenkinsTestApplication.java
+++ src/main/java/com/jenkins_test/JenkinsTestApplication.java
@@ -9,17 +9,18 @@ public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		System.out.println("application stated.......!");
-		System.out.println("application stated.111......!");
-		System.out.println("application stated....2222...!");
-		System.out.println("application stated.3333......!");
-		System.out.println("application stated..4444.....!");
-		System.out.println("application stated...55555....!");
+
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 
 	@PostConstruct
 	public void start(){
 		System.out.println("post construct is executed....!");
+		System.out.println("application stated.111......!");
+		System.out.println("application stated....2222...!");
+		System.out.println("application stated.3333......!");
+		System.out.println("application stated..4444.....!");
+		System.out.println("application stated...55555....!");
 	}
 
 }
